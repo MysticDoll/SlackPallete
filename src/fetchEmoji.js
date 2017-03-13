de6370912881a4a8c6e73b3f34f931b8c7cb1473
Mitsuru Takigahira <mysticdoll@mysticdoll.com>
2017-03-13 16:51:38 +0900

    Use XHR when window.fetch is not available

--- src/fetchEmoji.js
+++ src/fetchEmoji.js
@@ -1,4 +1,14 @@
 export default (token) =>
-  fetch(`https://slack.com/api/emoji.list?token=${token}`)
+  window.fetch ? fetch(`https://slack.com/api/emoji.list?token=${token}`)
     .then(r => r.json())
-    .then(r => r.emoji);
+    .then(r => r.emoji) : new Promise(function(resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", `https://slack.com/api/emoji.list?token=${token}`);
+      xhr.send();
+      xhr.addEventListener("load", function() {
+        resolve(JSON.parse(xhr.responseText).emoji);
+      });
+      xhr.addEventListener("error", function() {
+        reject(xhr.statusText);
+      });
+    });
